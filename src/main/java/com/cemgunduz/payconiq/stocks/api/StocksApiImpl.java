4e948d3fc9c54413e611fd240a85af66b0232c33
Cem Gunduz <cem@BB-SYSTEM-0396.local>
2017-12-29 14:38:46 +0200

    Added a postman collection for ease of testing

--- src/main/java/com/cemgunduz/payconiq/stocks/api/StocksApiImpl.java
+++ src/main/java/com/cemgunduz/payconiq/stocks/api/StocksApiImpl.java
@@ -7,16 +7,13 @@ import com.cemgunduz.payconiq.stocks.api.model.Stock;
 import com.cemgunduz.payconiq.stocks.persistence.StockDto;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
